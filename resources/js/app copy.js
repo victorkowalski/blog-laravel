93da8aaa9b99fe81603fc0b33c9e63149dbc10e1
vkowalsky <vkowalsky@ronasit.com>
2020-04-06 18:25:11 +0300

    vue initial commit

--- resources/js/app copy.js
+++ resources/js/app copy.js
@@ -1,16 +1,4 @@
-/*
-require('./bootstrap');
-
-    window.Vue = require('vue');
-
-    Vue.component('welcome', require('./components/Welcome.vue'));
-
-    const app = new Vue({
-        el: '#app'
-    });
-    */
-
-    /**
+/**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
@@ -31,8 +19,8 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('welcome', require('./components/Welcome.vue').default);
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
