4cb463e1a4cce48f4249f527e08b3db097d26365
vkowalsky <vkowalsky@ronasit.com>
2020-04-20 10:19:15 +0300

    api

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,29 +1,18 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html>
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/main.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <meta charset=utf-8>
+    <meta name=viewport content="width=device-width,initial-scale=1">
+    <title>blog-spa</title>
+    <link href="{{ asset('/static/css/app.52bc9c3372f8afe3865a72953242949b.css') }}" rel=stylesheet>
 </head>
-<body>
-     <noscript>
-      <strong>We're sorry but vue-2 doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
-    </noscript>
-    <div id="root"></div>
 
+<body>
+    <div id=root></div>
+    <script type=text/javascript src="{{ asset('/static/js/manifest.2ae2e69a05c33dfc65f8.js') }}"></script>
+    <script type=text/javascript src="{{ asset('/static/js/vendor.4ad267b4786a3ebd3327.js') }}"></script>
+    <script type=text/javascript src="{{ asset('/static/js/app.a286ab423ae5d51dd0b2.js') }}"></script>
 </body>
+
 </html>
